942a3a6e8c6e4d0046ea05125499910c5f424eda
Sajjaddi <sajjadjahanids@gmail.com>
2021-12-22 22:26:05 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+let $ = document;
+let body = $.querySelector("body");
+let inputSearch = $.querySelector(".searchbar input");
+let description = $.querySelector(".description");
+let btnSearch = $.querySelector(".icon-search");
+let titleWeather = $.querySelector(".title .value");
+let tempWeather = $.querySelector(".temp p");
+let statusWeather = $.querySelector(".icon-weather .value");
+let humidityWeather = $.querySelector(".humidity .value");
+let windSpeedWeather = $.querySelector(".wind-speed .value");
+let infoWeather = {
+  tehran: { temp: "12°", status: "Sunny", humidity: 23, windSpeed: "32km/h" },
+  rasht: { temp: "9°", status: "Windy", humidity: 12, windSpeed: "14km/h" },
+};
+btnSearch.addEventListener("click", function () {
+  let inputSearchVal = inputSearch.value;
+  inputSearchVal = inputSearchVal.toLowerCase();
+  let weatherResult = infoWeather[inputSearchVal];
+  if (weatherResult) {
+    description.style.display = "block";
+    inputSearchVal = inputSearchVal[0].toUpperCase() + inputSearchVal.slice(1);
+    titleWeather.textContent = inputSearchVal;
+    tempWeather.innerHTML = weatherResult.temp;
+    statusWeather.innerHTML = weatherResult.status;
+    humidityWeather.innerHTML = weatherResult.humidity;
+    windSpeedWeather.innerHTML = weatherResult.windSpeed;
+  } else {
+    description.style.display = "";
+    alert("incurrect!");
+  }
+});
+(function () {
+  let image = [
+    "/2082049.jpg",
+    "/daniele-buso-qzUenL35ZYw-unsplash.jpg",
+    "/nathan-queloz-VWZJgYLpmuI-unsplash.jpg",
+    "/zu-photography-JjAhzqZzk1M-unsplash.jpg",
+  ];
+  let randNum = Math.floor(Math.random() * image.length);
+  body.style.backgroundImage = `url('${image[randNum]}')`;
+})();
