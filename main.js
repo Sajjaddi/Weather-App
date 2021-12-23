daab240fb3ff5aa64b06a453176438646f23186f
Sajjaddi <sajjadjahanids@gmail.com>
2021-12-23 14:41:08 +0330

    add-comments

--- main.js
+++ main.js
@@ -18,7 +18,9 @@ btnSearch.addEventListener("click", function () {
   let weatherResult = infoWeather[inputSearchVal];
   if (weatherResult) {
     description.style.display = "block";
+    // نشون دادن بزرگ اولین کلمه شهر در عنوان
     inputSearchVal = inputSearchVal[0].toUpperCase() + inputSearchVal.slice(1);
+    // گرفتن مقادیر و اضافه کردن به صفحه
     titleWeather.textContent = inputSearchVal;
     tempWeather.innerHTML = weatherResult.temp;
     statusWeather.innerHTML = weatherResult.status;
@@ -36,6 +38,7 @@ btnSearch.addEventListener("click", function () {
     "nathan-queloz-VWZJgYLpmuI-unsplash.jpg",
     "zu-photography-JjAhzqZzk1M-unsplash.jpg",
   ];
+  // هر بار عددی از ۰ تا تعداد آرایه بالا انتخاب میشه رندوم و با ورود به صفحه عکس شانسی انتخاب میشود
   let randNum = Math.floor(Math.random() * image.length);
   body.style.backgroundImage = `url('${image[randNum]}')`;
 })();
